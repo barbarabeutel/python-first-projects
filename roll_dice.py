c41f0edead435e455db8741310c2ecff8bab15b2
barbarabeutel <102474882+barbarabeutel@users.noreply.github.com>
2022-09-25 15:35:35 +1000

    Delete roll_dice.py

--- roll_dice.py
+++
@@ -1,9 +0,0 @@
-import random
-
-roll = random.randint(1,6)
-guess = int(input('Guess the dice roll:\n'))
-
-if guess==roll:
-    print ('Slayed! You got it right! The dice rolled a ' + str(roll))
-else:
-    print('HA HA you wished but failed, they rolled a '+ str(roll))
